cf2ec38f816613b4974be0ab3645ed0eebab7305
Md. Saiful <saifulpro247@gmail.com>
2023-08-07 22:12:03 +0600

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,54 @@
 (function($) {
     "use strict";
 
-    // mobile menu
-    // Mobile Menu
-    $("#menu-active").on("click", function(event) {
-        event.stopPropagation();
-        $(".mobile_menus").slideToggle();
-    });
-    $(".has-menu").on("click", function(event) {
-        event.stopPropagation();
-        $(".sub-menu").slideToggle();
-    }); 
-    // Add click event listener to the body
-    $("body").on("click", function(event) {
-        if (!$(event.target).closest(".mobile_menus, #menu-active").length) {
-            $(".mobile_menus").slideUp();
-        }
-    });
+    $(document).ready(function() {
 
 
-    // select beautiy
-    $('select').niceSelect();
+        // mobile menu
+        $("#menu-active").on("click", function(event) {
+            event.stopPropagation();
+            $(".mobile_menus").slideToggle();
+        });
+        $(".has-menu").on("click", function(event) {
+            event.stopPropagation();
+            $(".sub-menu").slideToggle();
+        }); 
+        $("body").on("click", function(event) {
+            if (!$(event.target).closest(".mobile_menus, #menu-active").length) {
+                $(".mobile_menus").slideUp();
+            }
+        });
+
+        // select beautiy
+        $('select').niceSelect();
+
+        // Mobile packages slider
+        const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: false,
+        
+            // If we need pagination
+            pagination: {
+            el: '.swiper-pagination',
+            },
+        }); 
+
+        
+        // tab js
+        $('.tab-header').click(function() {
+            const tabId = $(this).data('tab');
+            $('.tab-header').removeClass('active');
+            $(this).addClass('active');
+            $('.tab-content').removeClass('active-tab').hide();
+            $('#' + tabId).addClass('active-tab').show();
+          });
+          $('.tab-header[data-tab="tab1"]').click();
+        
+      });
+
+
+
 
     
 
